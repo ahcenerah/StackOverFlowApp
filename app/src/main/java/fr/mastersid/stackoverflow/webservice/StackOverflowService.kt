ce109f42855e11300cd804ca42bddf960b38f771
Ahcene Rahab <ahcenerahab2@gmail.com>
2024-02-12 10:23:25 +0100

    mise en place des permissions

--- app/src/main/java/fr/mastersid/stackoverflow/webservice/StackOverflowService.kt
+++ app/src/main/java/fr/mastersid/stackoverflow/webservice/StackOverflowService.kt
@@ -1,5 +1,6 @@
 package fr.mastersid.stackoverflow.webservice
 
+import androidx.annotation.RequiresPermission
 import fr.mastersid.stackoverflow.data.ListQuestionJson
 import fr.mastersid.stackoverflow.data.Question
 import retrofit2.Call
@@ -7,6 +8,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface StackOverflowService {
+    @RequiresPermission( value = "android.permission.SEND_SMS")
     @GET("questions?pagesize=20&order=desc&sort=activity&site=stackoverflow")
     suspend fun getQuestionList():List<Question>
 }
